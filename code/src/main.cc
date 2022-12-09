dad7bb687b4ee8de0d3868b37d2487954aff6100
Ria Desai <rndesai2@illinos.edu>
2022-12-09 15:16:53 -0600

    commit on main

--- code/src/main.cc
+++ code/src/main.cc
@@ -5,10 +5,6 @@
 using namespace std;
 
 int main() {
-    cout << " jhkndsmla,; " << endl;
-    string file = "/Users/owner/Documents/cs225/225-project/ORRNFinalProject/code/facebook_combined (1).csv";
-    //readCSV(file, 4038);
-    //bfs(0, file);
-    primMST(file);
-    return 0;
+    cout << "Welcome! Input information when prompted to find out about Facebook users in our dataset!" << endl;
+    
 }
